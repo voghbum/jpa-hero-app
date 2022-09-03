08cb164a7d5cd729c649c09404563dccb38ca217
Hüseyin Aşcı <70473892+voghbum@users.noreply.github.com>
2022-09-03 17:27:43 +0300

    Initial commit

---
+++ src/main/java/com/voghbum/jpaheroapp/JpaHeroAppApplication.java
@@ -0,0 +1,11 @@
+package com.voghbum.jpaheroapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHeroAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHeroAppApplication.class, args);
+	}
+}
